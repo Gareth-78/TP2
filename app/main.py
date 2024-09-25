cd0e6309b3b72e76114aea3797f372706f67dcfa
gareth <carraragareth@gmail.com>
2024-09-25 14:15:24 +0200

    dockerfastapi

---
+++ app/main.py
@@ -0,0 +1,29 @@
+from typing import Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool=None
+
+@app.get("/")
+def read_root():
+    return {"Hello": "H3"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str=None):
+    return {"item_id":item_id, "q":q}
+
+
+@app.get("/gareth")
+def read_gareth():
+    return{"name":"Gareth"}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
